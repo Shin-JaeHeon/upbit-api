5b3915dc8f5c0cda8cf05244308b38a839557911
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-04 11:52:01 +0900

    fixed JSDocs;Fix algorithm errors;v0.1.1

--- index.ts
+++ index.ts
@@ -11,46 +11,49 @@ function ticker(market: Array<string>): Promise<Array<Market>> {
         };
         request(options, (error, response, body) => {
             if (error) reject(error);
-            const data = [];
-            body = JSON.parse(body.toString());
-            body.forEach(v => {
-                const market = new Market(v['market'].split('-')[0], v['market'].split('-')[1]);
-                market.tradeTime = new Date(v['trade_timestamp']);
-                market.price = v['trade_price'];
-                market.open = v['opening_price'];
-                market.high = v['high_price'];
-                market.low = v['low_price'];
-                market.prevClose = v['prev_closing_price'];
-                market.change = v['change'];
-                market.changePrice = v['change_price'];
-                market.changeRate = v['change_rate'];
-                market.signedChangePrice = v['signed_change_price'];
-                market.signedChangeRate = v['signed_change_rate'];
-                market.tradeVolume = v['trade_volume'];
-                market.accTradePrice = v['acc_trade_price'];
-                market.accTradePrice24 = v['acc_trade_price_24h'];
-                market.accTradeVolume = v['acc_trade_volume'];
-                market.accTradeVolume24 = v['acc_trade_volume_24h'];
-                market.high52wPrice = v['highest_52_week_price'];
-                market.high52wDate = new Date(v['highest_52_week_date']);
-                market.low52wPrice = v['lowest_52_week_price'];
-                market.low52wDate = new Date(v['lowest_52_week_date']);
-                market.lastUpdate = new Date();
-                data.push(market);
-            });
-            resolve(data);
+            else {
+                const data = [];
+                body = JSON.parse(body.toString());
+                body.forEach(v => {
+                    const market = new Market(v['market'].split('-')[0], v['market'].split('-')[1]);
+                    market.tradeTime = new Date(v['trade_timestamp']);
+                    market.price = v['trade_price'];
+                    market.open = v['opening_price'];
+                    market.high = v['high_price'];
+                    market.low = v['low_price'];
+                    market.prevClose = v['prev_closing_price'];
+                    market.change = v['change'];
+                    market.changePrice = v['change_price'];
+                    market.changeRate = v['change_rate'];
+                    market.signedChangePrice = v['signed_change_price'];
+                    market.signedChangeRate = v['signed_change_rate'];
+                    market.tradeVolume = v['trade_volume'];
+                    market.accTradePrice = v['acc_trade_price'];
+                    market.accTradePrice24 = v['acc_trade_price_24h'];
+                    market.accTradeVolume = v['acc_trade_volume'];
+                    market.accTradeVolume24 = v['acc_trade_volume_24h'];
+                    market.high52wPrice = v['highest_52_week_price'];
+                    market.high52wDate = new Date(v['highest_52_week_date']);
+                    market.low52wPrice = v['lowest_52_week_price'];
+                    market.low52wDate = new Date(v['lowest_52_week_date']);
+                    market.lastUpdate = new Date();
+                    data.push(market);
+                });
+                resolve(data);
+            }
         });
     });
 }
 
 /**
  *
- * @param market Object to update
+ * Updates object [market] every specified [time] time.
+ * @param market An object to update
  * @param time update interval(ms)
- * @param errorHandler
- * @param callback called when updated
+ * @param {(error) => any} errorHandler
+ * @param {(market) => any} callback called when updated
  */
-function autoUpdate(market: Market, time: number, errorHandler: Function, callback?: Function): void {
+function autoUpdate(market: Market, time: number, errorHandler: (error) => any, callback?: (market) => any): void {
     setInterval(() => {
         const options = {
             method: 'GET',
@@ -59,29 +62,31 @@ function autoUpdate(market: Market, time: number, errorHandler: Function, callba
         };
         request(options, (error, response, body) => {
             if (error) errorHandler(error);
-            body = JSON.parse(body.toString())[0];
-            market.tradeTime = new Date(body['trade_timestamp']);
-            market.price = body['trade_price'];
-            market.open = body['opening_price'];
-            market.high = body['high_price'];
-            market.low = body['low_price'];
-            market.prevClose = body['prev_closing_price'];
-            market.change = body['change'];
-            market.changePrice = body['change_price'];
-            market.changeRate = body['change_rate'];
-            market.signedChangePrice = body['signed_change_price'];
-            market.signedChangeRate = body['signed_change_rate'];
-            market.tradeVolume = body['trade_volume'];
-            market.accTradePrice = body['acc_trade_price'];
-            market.accTradePrice24 = body['acc_trade_price_24h'];
-            market.accTradeVolume = body['acc_trade_volume'];
-            market.accTradeVolume24 = body['acc_trade_volume_24h'];
-            market.high52wPrice = body['highest_52_week_price'];
-            market.high52wDate = new Date(body['highest_52_week_date']);
-            market.low52wPrice = body['lowest_52_week_price'];
-            market.low52wDate = new Date(body['lowest_52_week_date']);
-            market.lastUpdate = new Date();
-            if (callback) callback(market);
+            else {
+                body = JSON.parse(body.toString())[0];
+                market.tradeTime = new Date(body['trade_timestamp']);
+                market.price = body['trade_price'];
+                market.open = body['opening_price'];
+                market.high = body['high_price'];
+                market.low = body['low_price'];
+                market.prevClose = body['prev_closing_price'];
+                market.change = body['change'];
+                market.changePrice = body['change_price'];
+                market.changeRate = body['change_rate'];
+                market.signedChangePrice = body['signed_change_price'];
+                market.signedChangeRate = body['signed_change_rate'];
+                market.tradeVolume = body['trade_volume'];
+                market.accTradePrice = body['acc_trade_price'];
+                market.accTradePrice24 = body['acc_trade_price_24h'];
+                market.accTradeVolume = body['acc_trade_volume'];
+                market.accTradeVolume24 = body['acc_trade_volume_24h'];
+                market.high52wPrice = body['highest_52_week_price'];
+                market.high52wDate = new Date(body['highest_52_week_date']);
+                market.low52wPrice = body['lowest_52_week_price'];
+                market.low52wDate = new Date(body['lowest_52_week_date']);
+                market.lastUpdate = new Date();
+                if (callback) callback(market);
+            }
         });
     }, time);
 }
