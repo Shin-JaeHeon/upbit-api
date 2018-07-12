420296ced96a2d1102913353043a89c9104e1b6e
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-12 21:51:42 +0900

    refactoring code: set data to Market class.

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const Market_1 = require("./container/Market");
 const OrderBook_1 = require("./container/OrderBook");
 const Order_1 = require("./container/Order");
 const request = require("request");
+function setMarketData(market, v) {
+    market.tradeTime = new Date(v['trade_timestamp']);
+    market.price = v['trade_price'];
+    market.open = v['opening_price'];
+    market.high = v['high_price'];
+    market.low = v['low_price'];
+    market.prevClose = v['prev_closing_price'];
+    market.change = v['change'];
+    market.changePrice = v['change_price'];
+    market.changeRate = v['change_rate'];
+    market.signedChangePrice = v['signed_change_price'];
+    market.signedChangeRate = v['signed_change_rate'];
+    market.tradeVolume = v['trade_volume'];
+    market.accTradePrice = v['acc_trade_price'];
+    market.accTradePrice24 = v['acc_trade_price_24h'];
+    market.accTradeVolume = v['acc_trade_volume'];
+    market.accTradeVolume24 = v['acc_trade_volume_24h'];
+    market.high52wPrice = v['highest_52_week_price'];
+    market.high52wDate = new Date(v['highest_52_week_date']);
+    market.low52wPrice = v['lowest_52_week_price'];
+    market.low52wDate = new Date(v['lowest_52_week_date']);
+    market.lastUpdate = new Date();
+}
 /**
  * Create new Market object arrays.
  * @param market example: ['KRW-BTC', 'KRW-XRP']
@@ -23,27 +46,7 @@ function ticker(market) {
                 body = JSON.parse(body.toString());
                 body.forEach(v => {
                     const market = new Market_1.default(v['market'].split('-')[0], v['market'].split('-')[1]);
-                    market.tradeTime = new Date(v['trade_timestamp']);
-                    market.price = v['trade_price'];
-                    market.open = v['opening_price'];
-                    market.high = v['high_price'];
-                    market.low = v['low_price'];
-                    market.prevClose = v['prev_closing_price'];
-                    market.change = v['change'];
-                    market.changePrice = v['change_price'];
-                    market.changeRate = v['change_rate'];
-                    market.signedChangePrice = v['signed_change_price'];
-                    market.signedChangeRate = v['signed_change_rate'];
-                    market.tradeVolume = v['trade_volume'];
-                    market.accTradePrice = v['acc_trade_price'];
-                    market.accTradePrice24 = v['acc_trade_price_24h'];
-                    market.accTradeVolume = v['acc_trade_volume'];
-                    market.accTradeVolume24 = v['acc_trade_volume_24h'];
-                    market.high52wPrice = v['highest_52_week_price'];
-                    market.high52wDate = new Date(v['highest_52_week_date']);
-                    market.low52wPrice = v['lowest_52_week_price'];
-                    market.low52wDate = new Date(v['lowest_52_week_date']);
-                    market.lastUpdate = new Date();
+                    setMarketData(market, v);
                     data.push(market);
                 });
                 resolve(data);
@@ -68,27 +71,7 @@ function autoUpdate(market, time, errorHandler, callback) {
                 errorHandler(error);
             else {
                 body = JSON.parse(body.toString())[0];
-                market.tradeTime = new Date(body['trade_timestamp']);
-                market.price = body['trade_price'];
-                market.open = body['opening_price'];
-                market.high = body['high_price'];
-                market.low = body['low_price'];
-                market.prevClose = body['prev_closing_price'];
-                market.change = body['change'];
-                market.changePrice = body['change_price'];
-                market.changeRate = body['change_rate'];
-                market.signedChangePrice = body['signed_change_price'];
-                market.signedChangeRate = body['signed_change_rate'];
-                market.tradeVolume = body['trade_volume'];
-                market.accTradePrice = body['acc_trade_price'];
-                market.accTradePrice24 = body['acc_trade_price_24h'];
-                market.accTradeVolume = body['acc_trade_volume'];
-                market.accTradeVolume24 = body['acc_trade_volume_24h'];
-                market.high52wPrice = body['highest_52_week_price'];
-                market.high52wDate = new Date(body['highest_52_week_date']);
-                market.low52wPrice = body['lowest_52_week_price'];
-                market.low52wDate = new Date(body['lowest_52_week_date']);
-                market.lastUpdate = new Date();
+                setMarketData(market, body);
                 if (callback)
                     callback(market);
             }
--- index.ts
+++ index.ts
@@ -4,51 +4,55 @@ import Order from "./container/Order";
 
 const request = require("request");
 
+function setMarketData(market, v) {
+  market.tradeTime = new Date(v['trade_timestamp']);
+  market.price = v['trade_price'];
+  market.open = v['opening_price'];
+  market.high = v['high_price'];
+  market.low = v['low_price'];
+  market.prevClose = v['prev_closing_price'];
+  market.change = v['change'];
+  market.changePrice = v['change_price'];
+  market.changeRate = v['change_rate'];
+  market.signedChangePrice = v['signed_change_price'];
+  market.signedChangeRate = v['signed_change_rate'];
+  market.tradeVolume = v['trade_volume'];
+  market.accTradePrice = v['acc_trade_price'];
+  market.accTradePrice24 = v['acc_trade_price_24h'];
+  market.accTradeVolume = v['acc_trade_volume'];
+  market.accTradeVolume24 = v['acc_trade_volume_24h'];
+  market.high52wPrice = v['highest_52_week_price'];
+  market.high52wDate = new Date(v['highest_52_week_date']);
+  market.low52wPrice = v['lowest_52_week_price'];
+  market.low52wDate = new Date(v['lowest_52_week_date']);
+  market.lastUpdate = new Date();
+}
+
 /**
  * Create new Market object arrays.
  * @param market example: ['KRW-BTC', 'KRW-XRP']
  */
 function ticker(market: Array<string>): Promise<Array<Market>> {
-    return new Promise((resolve, reject) => {
-        const options = {
-            method: 'GET',
-            url: 'https://api.upbit.com/v1/ticker',
-            qs: {markets: market.toString()}
-        };
-        request(options, (error, response, body) => {
-            if (error) reject(error);
-            else {
-                const data = [];
-                body = JSON.parse(body.toString());
-                body.forEach(v => {
-                    const market = new Market(v['market'].split('-')[0], v['market'].split('-')[1]);
-                    market.tradeTime = new Date(v['trade_timestamp']);
-                    market.price = v['trade_price'];
-                    market.open = v['opening_price'];
-                    market.high = v['high_price'];
-                    market.low = v['low_price'];
-                    market.prevClose = v['prev_closing_price'];
-                    market.change = v['change'];
-                    market.changePrice = v['change_price'];
-                    market.changeRate = v['change_rate'];
-                    market.signedChangePrice = v['signed_change_price'];
-                    market.signedChangeRate = v['signed_change_rate'];
-                    market.tradeVolume = v['trade_volume'];
-                    market.accTradePrice = v['acc_trade_price'];
-                    market.accTradePrice24 = v['acc_trade_price_24h'];
-                    market.accTradeVolume = v['acc_trade_volume'];
-                    market.accTradeVolume24 = v['acc_trade_volume_24h'];
-                    market.high52wPrice = v['highest_52_week_price'];
-                    market.high52wDate = new Date(v['highest_52_week_date']);
-                    market.low52wPrice = v['lowest_52_week_price'];
-                    market.low52wDate = new Date(v['lowest_52_week_date']);
-                    market.lastUpdate = new Date();
-                    data.push(market);
-                });
-                resolve(data);
-            }
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.upbit.com/v1/ticker',
+      qs: {markets: market.toString()}
+    };
+    request(options, (error, response, body) => {
+      if (error) reject(error);
+      else {
+        const data = [];
+        body = JSON.parse(body.toString());
+        body.forEach(v => {
+          const market = new Market(v['market'].split('-')[0], v['market'].split('-')[1]);
+          setMarketData(market, v);
+          data.push(market);
         });
+        resolve(data);
+      }
     });
+  });
 }
 
 /**
@@ -59,41 +63,21 @@ function ticker(market: Array<string>): Promise<Array<Market>> {
  * @param {(market) => any} callback called when updated
  */
 function autoUpdate(market: any, time: number, errorHandler: (error) => any, callback?: (market) => any): void {
-    const run = market => setInterval(() => {
-        const options = {
-            method: 'GET', url: 'https://api.upbit.com/v1/ticker', qs: {markets: `${market.market}-${market.coin}`}
-        };
-        request(options, (error, response, body) => {
-            if (error) errorHandler(error);
-            else {
-                body = JSON.parse(body.toString())[0];
-                market.tradeTime = new Date(body['trade_timestamp']);
-                market.price = body['trade_price'];
-                market.open = body['opening_price'];
-                market.high = body['high_price'];
-                market.low = body['low_price'];
-                market.prevClose = body['prev_closing_price'];
-                market.change = body['change'];
-                market.changePrice = body['change_price'];
-                market.changeRate = body['change_rate'];
-                market.signedChangePrice = body['signed_change_price'];
-                market.signedChangeRate = body['signed_change_rate'];
-                market.tradeVolume = body['trade_volume'];
-                market.accTradePrice = body['acc_trade_price'];
-                market.accTradePrice24 = body['acc_trade_price_24h'];
-                market.accTradeVolume = body['acc_trade_volume'];
-                market.accTradeVolume24 = body['acc_trade_volume_24h'];
-                market.high52wPrice = body['highest_52_week_price'];
-                market.high52wDate = new Date(body['highest_52_week_date']);
-                market.low52wPrice = body['lowest_52_week_price'];
-                market.low52wDate = new Date(body['lowest_52_week_date']);
-                market.lastUpdate = new Date();
-                if (callback) callback(market);
-            }
-        });
-    }, time);
-    if (Array.isArray(market)) market.forEach(v => run(v));
-    else run(market);
+  const run = market => setInterval(() => {
+    const options = {
+      method: 'GET', url: 'https://api.upbit.com/v1/ticker', qs: {markets: `${market.market}-${market.coin}`}
+    };
+    request(options, (error, response, body) => {
+      if (error) errorHandler(error);
+      else {
+        body = JSON.parse(body.toString())[0];
+        setMarketData(market, body);
+        if (callback) callback(market);
+      }
+    });
+  }, time);
+  if (Array.isArray(market)) market.forEach(v => run(v));
+  else run(market);
 
 }
 
@@ -102,28 +86,28 @@ function autoUpdate(market: any, time: number, errorHandler: (error) => any, cal
  * @param market
  */
 function orderBook(market: any): Promise<Array<OrderBook>> {
-    return new Promise((resolve, reject) => {
-        request({
-            method: 'GET',
-            url: 'https://api.upbit.com/v1/orderbook',
-            qs: {markets: market.toString()}
-        }, (error, response, body) => {
-            body = JSON.parse(body);
-            resolve(body.map(v => {
-                    const order = new OrderBook(v['market'].split('-')[0], v['market'].split('-')[1]);
-                    const ask = [];
-                    const bid = [];
-                    v['orderbook_units'].forEach(v => {
-                        ask.push(new Order(v['ask_price'], v['ask_size']));
-                        bid.push(new Order(v['bid_price'], v['bid_size']));
-                    });
-                    order.askList = ask;
-                    order.bidList = bid;
-                    return order;
-                }
-            ));
-        });
+  return new Promise((resolve, reject) => {
+    request({
+      method: 'GET',
+      url: 'https://api.upbit.com/v1/orderbook',
+      qs: {markets: market.toString()}
+    }, (error, response, body) => {
+      body = JSON.parse(body);
+      resolve(body.map(v => {
+          const order = new OrderBook(v['market'].split('-')[0], v['market'].split('-')[1]);
+          const ask = [];
+          const bid = [];
+          v['orderbook_units'].forEach(v => {
+            ask.push(new Order(v['ask_price'], v['ask_size']));
+            bid.push(new Order(v['bid_price'], v['bid_size']));
+          });
+          order.askList = ask;
+          order.bidList = bid;
+          return order;
+        }
+      ));
     });
+  });
 }
 
 export default {ticker, autoUpdate, orderBook}
