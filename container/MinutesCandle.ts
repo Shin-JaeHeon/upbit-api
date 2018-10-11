36537a3fe8528a06d685e2676512f3723b9e70d9
Shin-JaeHeon <heoni0504@gmail.com>
2018-10-11 12:00:59 +0900

    ADD > MinutesCandle

---
+++ container/MinutesCandle.ts
@@ -0,0 +1,15 @@
+import Candle from './Candle';
+
+export default class MinutesCandle extends Candle {
+
+  private _unit: number;
+
+
+  get unit(): number {
+    return this._unit;
+  }
+
+  set unit(value: number) {
+    this._unit = value;
+  }
+}
